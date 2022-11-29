b3aaff0b2ee7b0007671071e3cc8882a9f884396
Leonid Sukhin <leva1981@yandex.ru>
2022-11-29 15:25:38 +0300

    refactor CategoryDto annotations

--- src/main/java/ru/jsft/gtdfan/controller/dto/CategoryDto.java
+++ src/main/java/ru/jsft/gtdfan/controller/dto/CategoryDto.java
@@ -1,15 +1,16 @@
 package ru.jsft.gtdfan.controller.dto;
 
-import lombok.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import javax.validation.constraints.NotBlank;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@EqualsAndHashCode(callSuper = true)
+@Data
+@Builder
 @ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
 public class CategoryDto extends AbstractDto {
 
     @NotBlank(message = "Category must have a name")
