9197765cd66f1b9a1164e10aa532104a352b1923
Leonid Sukhin <leva1981@yandex.ru>
2022-11-30 16:46:50 +0300

    create PopulateTasks

--- src/main/java/ru/jsft/gtdfan/config/PopulateTasks.java
+++ src/main/java/ru/jsft/gtdfan/config/PopulateTasks.java
@@ -3,16 +3,99 @@ package ru.jsft.gtdfan.config;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import ru.jsft.gtdfan.repository.NoteRepository;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
+import org.springframework.transaction.annotation.Transactional;
+import ru.jsft.gtdfan.model.Category;
+import ru.jsft.gtdfan.model.Priority;
+import ru.jsft.gtdfan.model.Task;
+import ru.jsft.gtdfan.model.User;
+import ru.jsft.gtdfan.repository.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Configuration
 public class PopulateTasks {
 
-    @Bean
-    CommandLineRunner commandLineRunner(NoteRepository repository) {
-        return args -> {
-            // TODO: implement some actions or delete class
+    private final UserRepository userRepository;
+    private final PriorityRepository priorityRepository;
+    private final CategoryRepository categoryRepository;
+    private final TaskRepository taskRepository;
+    private final NoteRepository noteRepository;
 
+    public PopulateTasks(UserRepository userRepository,
+                         PriorityRepository priorityRepository,
+                         CategoryRepository categoryRepository,
+                         TaskRepository taskRepository,
+                         NoteRepository noteRepository) {
+
+        this.userRepository = userRepository;
+        this.priorityRepository = priorityRepository;
+        this.categoryRepository = categoryRepository;
+        this.taskRepository = taskRepository;
+        this.noteRepository = noteRepository;
+    }
+
+    @Bean
+    @Transactional
+    CommandLineRunner commandLineRunner() {
+//        INSERT INTO users(id, name, email, password)
+//        VALUES (1, 'Leonid', 'leva1981@yandex.ru', 'admin'),
+//                (2, 'Natasha', 'natasuhina@yandex.ru', 'user');
+//
+//        INSERT INTO priority(id, name, level)
+//        VALUES (1, 'High', 0),
+//                (2, 'Middle', 5),
+//        (3, 'Low', 10),
+//        (4, 'none', 1000);
+//
+//        INSERT INTO category(id, name)
+//        VALUES (1, '1 - Today'),
+//                (2, '2 - Week');
+//
+//        INSERT INTO task(id, name, until, link, edited, category_id, priority_id, supertask_id, user_id)
+//        VALUES (1, 'Проверь обновление 1С (еженедельно)', null, null, now(), 1, 1, null, 1),
+//                (2, 'Орион: сбой резервного копирования!', null, null, now(), 1, 1, null, 1),
+//        (3, 'Воронение стали', null, 'https://youtu.be/1GKftAi4gXo', now(), 1, 2, null, 1),
+//        (4, 'Учебное видео: Sasgis, Ozi, распечатка. Полезные ресурсы.', null, null, now(), 1, 2, null, 1),
+//        (5, 'Флешка с музыкой', '2022-11-15', null, now(), 1, 3, null, 1),
+//        (6, 'Видео с конференции JPoint...', null, 'https://www.youtube.com/playlist?list=PLVe-2wcL84b8OCdXV_tqP8YrMIlgB_BER', now(), 1, 4, null, 1),
+//        (7, 'Some holdover task', '2023-06-01', null, now(), 2, 2, null, 1),
+//        (8, 'Subtask for 1C update', null, null, now(), 1, 4, 1, 1),
+//        (9, 'Some task by Natasha', null, null, now(), 1, 2, null, 2),
+//        (10, 'Some holdover task by Natasha', '2024-01-01', null, now(), 2, 2, null, 2);
+//
+//        INSERT INTO note(id, task_id, note_text)
+//        VALUES (1, 1, 'Note for 1C update'),
+//                (2, 2, 'Another one note for update ')
+        return args -> {
+            User userLeonid = new User("Leonid", "leva@ya.ru", "admin", LocalDateTime.now(), true);
+            User userNatasha = new User("Natasha", "ns@ya.ru", "user", LocalDateTime.now(), true);
+            userRepository.saveAll(List.of(userLeonid, userNatasha));
+
+            Priority high = new Priority("High", 0);
+            Priority middle = new Priority("Middle", 5);
+            Priority low = new Priority("Low", 10);
+            Priority none = new Priority("None", 1000);
+            priorityRepository.saveAll(List.of(high, middle, low, none));
+
+            Category today = new Category("1 - Today");
+            Category week = new Category("2 - Week");
+            categoryRepository.saveAll(List.of(today, week));
+
+            taskRepository.saveAll(List.of(
+                    new Task(false,
+                            "Проверь обновление 1С (еженедельно)",
+                            null,
+                            null,
+                            LocalDateTime.now(),
+                            LocalDateTime.now(),
+                            today,
+                            high,
+                            null,
+                            AggregateReference.to(userLeonid.getId()),
+                            null
+            )));
         };
     }
 }
