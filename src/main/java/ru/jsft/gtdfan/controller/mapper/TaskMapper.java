c397aa164022c478a2c57da62f72c65823702bf9
Leonid Sukhin <leva1981@yandex.ru>
2022-11-30 16:50:02 +0300

    change MappersTest and TaskMapper to ignore created and updated fields

--- src/main/java/ru/jsft/gtdfan/controller/mapper/TaskMapper.java
+++ src/main/java/ru/jsft/gtdfan/controller/mapper/TaskMapper.java
@@ -1,6 +1,7 @@
 package ru.jsft.gtdfan.controller.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import ru.jsft.gtdfan.controller.dto.TaskDto;
 import ru.jsft.gtdfan.model.Task;
@@ -8,6 +9,9 @@ import ru.jsft.gtdfan.model.Task;
 @Mapper(uses = {AggregateUserLongConverter.class})
 public interface TaskMapper {
     TaskMapper INSTANCE = Mappers.getMapper(TaskMapper.class);
+
+    @Mapping(ignore = true, target = "created")
+    @Mapping(ignore = true, target = "updated")
     Task toEntity(TaskDto destination);
     TaskDto toDto(Task source);
 }
--- src/test/java/ru/jsft/gtdfan/mapper/MappersTest.java
+++ src/test/java/ru/jsft/gtdfan/mapper/MappersTest.java
@@ -31,12 +31,12 @@ public class MappersTest {
         Task task = Instancio.create(Task.class);
         assertThat(TaskMapper.INSTANCE.toEntity(TaskMapper.INSTANCE.toDto(task)))
                 .isNotNull()
-                .usingRecursiveComparison().isEqualTo(task);
+                .usingRecursiveComparison().ignoringFields("created", "updated").isEqualTo(task);
         // check with userId != null
         task.setUserId(AggregateReference.to(1L));
         assertThat(TaskMapper.INSTANCE.toEntity(TaskMapper.INSTANCE.toDto(task)))
                 .isNotNull()
-                .usingRecursiveComparison().isEqualTo(task);
+                .usingRecursiveComparison().ignoringFields("created", "updated").isEqualTo(task);
 
         User user = Instancio.create(User.class);
         assertThat(UserMapper.INSTANCE.toEntity(UserMapper.INSTANCE.toDto(user)))
