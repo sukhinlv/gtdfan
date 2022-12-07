6dd47bb3544d3fb021d330b0f615eef649c3b161
Leonid Sukhin <leva1981@yandex.ru>
2022-12-07 16:39:01 +0300

    add H2 Tcp Server

--- src/main/java/ru/jsft/gtdfan/config/AppConfiguration.java
+++ src/main/java/ru/jsft/gtdfan/config/AppConfiguration.java
@@ -1,9 +1,22 @@
 package ru.jsft.gtdfan.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
+
+import java.sql.SQLException;
 
 @Configuration
 @EnableJdbcAuditing
+@Slf4j
 class AppConfiguration {
+    @Profile("!test")
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    Server h2Server() throws SQLException {
+        log.info("Start H2 TCP server");
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+    }
 }
